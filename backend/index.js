46ac1b6c38d14a1740f73de09ed142db12cab4c9
Ayush Verma <123292445+ayuu-te@users.noreply.github.com>
2024-10-04 00:09:54 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express" ;
+import dotenv from 'dotenv' ;
+import connectDb from "./database/db.js";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("server working");
+});
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+    connectDb();
+});
